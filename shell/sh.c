cfd02b3407e757e0d04500c365a43383ebd51004
Will Jackson <williamjackson2020@gmail.com>
2026-01-21 13:14:16 -0700

    shell starter code

---
+++ shell/sh.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+  sh_loop();
+  return EXIT_SUCCESS;
+}
